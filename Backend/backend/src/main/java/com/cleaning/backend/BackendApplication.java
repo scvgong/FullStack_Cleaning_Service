f306bca3f860c0bdb8b64c83de9ed4aec5d182dc
scvgong <gongmyeongyun@gmail.com>
2025-04-08 00:47:40 +0900

    mysql 세팅

--- Backend/backend/src/main/java/com/cleaning/backend/BackendApplication.java
+++ Backend/backend/src/main/java/com/cleaning/backend/BackendApplication.java
@@ -1,7 +1,12 @@
 package com.cleaning.backend;
 
+import jakarta.annotation.PostConstruct;
+import org.apache.ibatis.mapping.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +14,6 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
 
 }
