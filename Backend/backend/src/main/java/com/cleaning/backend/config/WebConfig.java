1fcdd10a3fababce09b719667cd451f33225d51e
scvgong <gongmyeongyun@gmail.com>
2025-04-10 12:34:27 +0900

    관리자 페이지 상세화면 이미지 출력

--- Backend/backend/src/main/java/com/cleaning/backend/config/WebConfig.java
+++ Backend/backend/src/main/java/com/cleaning/backend/config/WebConfig.java
@@ -1,10 +1,17 @@
 package com.cleaning.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
+import java.io.File;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -12,5 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        String fullPath = "file:" + new File(uploadDir).getAbsolutePath() + "/";
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(fullPath);
     }
 }
