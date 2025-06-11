db193f1d3c01b092c0844a5200a345f7641bf141
scvgong <151099297+scvgong@users.noreply.github.com>
2025-06-11 10:45:49 +0900

    Add files via upload

--- Frontend/src/layouts/AdminLayout.jsx
+++ Frontend/src/layouts/AdminLayout.jsx
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import AdminHeader from "../components/AdminHeader";
 
 export default function AdminLayout() {
+  const location = useLocation();
+
+  //헤더.레이아웃 숨길 경로
+  const hiddenLayoutPaths = ["/admin/login", "/admin/register"];
+  const isLayoutHidden = hiddenLayoutPaths.includes(location.pathname);
+
+  if(isLayoutHidden){
+    return <Outlet />;
+  }
+
   return (
     <>
       <AdminHeader /> {/* 관리자 전용 Nav */}
--- Frontend/src/layouts/BizLayout.jsx
+++ Frontend/src/layouts/BizLayout.jsx
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import BizHeader from "../components/BizHeader";
 
 export default function BizLayout() {
+  const location = useLocation();
+
+  //헤더 레이아웃 숨길 경로
+  const hiddenLayoutPaths = ["/business/login", "/business/register"];
+  const isLayoutHidden = hiddenLayoutPaths.includes(location.pathname);
+
+  if(isLayoutHidden){
+    return <Outlet />;
+  }
+
   return (
     <>
       <BizHeader /> {/* 사업자 전용 Nav */}
